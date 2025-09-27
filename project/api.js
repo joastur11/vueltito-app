05aac9750a24947ca0a8dfd91b498882f7ddcc3f
Joaquin Stur <joaquinstur@gmail.com>
2025-09-27 13:09:02 -0300

    fetch pa probar

---
+++ project/api.js
@@ -0,0 +1,7 @@
+export { CotizarDolares }
+
+async function CotizarDolares() {
+  const respuesta = await fetch('https://dolarapi.com/v1/dolares')
+  if (!respuesta.ok) throw new Error('Error en el fetch')
+  return await respuesta.json()
+}
