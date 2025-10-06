dcd3a1b1213ccb68af14eb6d56ac37f4a0647985
Joaquin Stur <joaquinstur@gmail.com>
2025-10-06 11:41:18 -0300

    codigo cripto y plazo fijo

--- project/main.js
+++ project/main.js
@@ -1,16 +1,16 @@
-import { cotizarDolares, obtenerPrecioOroUsd, obtenerTNA } from "./api.js";
+import { cotizarDolares, obtenerPrecioOroUsd, obtenerTNA, cotizarDolarCripto, obtenerPlazosFijos } from "./api.js";
 
-
+// dolares
 async function mostrarDolares() {
   const $divDolares = document.querySelector('#cotizacion-dolares')
 
   const cotizacion = await cotizarDolares()
 
   $divDolares.innerHTML = `
-    <p> Dolar </p>
+    <strong> Dolar </strong>
     <ul>
-      <li> ${cotizacion[0].nombre} - Compra: ${cotizacion[0].compra} - Venta: ${cotizacion[0].venta} </li>
-      <li> ${cotizacion[1].nombre} - Compra: ${cotizacion[1].compra} - Venta: ${cotizacion[1].venta} </li>
+      <li> ${cotizacion[0].nombre} - Compra: $${cotizacion[0].compra} - Venta: $${cotizacion[0].venta} </li>
+      <li> ${cotizacion[1].nombre} - Compra: $${cotizacion[1].compra} - Venta: $${cotizacion[1].venta} </li>
     </ul>    
   `
 }
@@ -34,23 +34,61 @@ async function comprarDolares() {
   const $divCompraDolares = document.querySelector('#compra-dolares')
 
   $divCompraDolares.innerHTML = `
-    <p> Con tus ${platita} pesos, podes comprar: </p>
+    <p> Con tus $${platita} pesos, podes comprar: </p>
     <ul>
-      <li>Dolar Oficial: ${compraOficial} </li>
-      <li>Dolar Blue: ${compraBlue} </li>
+      <li>Dolar Oficial: $${compraOficial} </li>
+      <li>Dolar Blue: $${compraBlue} </li>
     </ul>
   `
 }
 
+// dolar cripto
+async function mostrarDolarCripto() {
+  const $divDolares = document.querySelector('#cotizacion-dolar-cripto')
+
+  const cotizacion = await cotizarDolarCripto()
+
+  $divDolares.innerHTML = `
+    <strong> Dolar Cripto </strong>
+    <ul>
+      <li> ${cotizacion.nombre} - Compra: $${cotizacion.compra} - Venta: $${cotizacion.venta} </li>
+    </ul>    
+  `
+}
+mostrarDolarCripto()
+
+async function calcularDolarCripto() {
+  const platita = $montoUsuario.value
+  const cotizacion = await cotizarDolarCripto()
+
+  const compraCripto = (platita / cotizacion.compra).toFixed(2)
+
+  return { platita, compraCripto }
+}
+
+async function comprarDolarCripto() {
+  const { platita, compraCripto } = await calcularDolarCripto()
+
+  const $divCompraDolarCripto = document.querySelector('#compra-dolar-cripto')
+
+  $divCompraDolarCripto.innerHTML = `
+    <p> Con tus $${platita} pesos, podes comprar: </p>
+    <ul>
+      <li>Dolar Cripto: $${compraCripto} </li>
+    </ul>
+  `
+}
+
+// Oro
 async function mostrarOro() {
   const $divOro = document.querySelector('#cotizacion-oro')
 
   const cotizacion = await obtenerPrecioOroUsd()
 
   $divOro.innerHTML = `
-    <p> Oro: 1oz </p>
+    <strong> Oro: 1oz </strong>
     <ul>
-      <li> ${cotizacion} Dolares </li>
+      <li> $${cotizacion} Dolares </li>
     </ul>
   `
 }
@@ -72,21 +110,21 @@ async function comprarOro() {
   const $divCompraOro = document.querySelector('#compra-oro')
 
   $divCompraOro.innerHTML = `
-    <p> Con tus ${platita} pesos, podes comprar: </p>
+    <p> Con tus $${platita} pesos, podes comprar: </p>
     <ul>
       <li>Onzas de Oro: ${compraOro.toFixed(4)} - Unos ${enGramos.toFixed(2)} gramos. </li>
     </ul>
   `
 }
 
-// inversiones
+// billeteras
 
 async function mostrarTazas() {
   const tnas = await obtenerTNA()
   const $tnas = document.querySelector('#tnas')
 
   $tnas.innerHTML = `
-    <p> Rendimientos en Billeteras </p>
+    <strong> Rendimientos en Billeteras </strong>
     <ul>
       ${tnas.map(r => `<li> ${r.nombre}: ${r.tna}% anual </li>`).join('')}
     </ul>
@@ -114,17 +152,62 @@ async function mostrarRendimientos() {
   const rendimientos = await calcularTNA()
 
   $divRendimientos.innerHTML = `
-    <p>Con tus ${$montoUsuario.value} pesos, al mes ganarías:</p>
+    <p>Con tus $${$montoUsuario.value} pesos, al mes ganarías:</p>
     <ul>
       ${rendimientos.map(r => `<li>${r.nombre}: $${r.rendimiento}</li>`).join('')}
     </ul>
   `
 }
 
+// plazos fijos 
+
+async function mostrarPlazosFijos() {
+  const plazos = await obtenerPlazosFijos()
+  const $plazos = document.querySelector('#tazas')
+
+  $plazos.innerHTML = `
+    <strong> Plazo fijo a 30 días </strong>
+    <ul>
+      ${plazos.map(r => `<li> ${r.nombre}: ${r.tna}% anual </li>`).join('')}
+    </ul>
+  `
+}
+mostrarPlazosFijos()
+
+async function calcularPlazosFijos() {
+  const plazos = await obtenerPlazosFijos()
+  const platita = Number($montoUsuario.value)
+
+  return plazos.map(inversion => {
+    const tasaMensual = inversion.tna / 12 / 100
+    const rendimiento = platita * tasaMensual
+
+    return {
+      nombre: inversion.nombre,
+      rendimiento: rendimiento.toFixed(2)
+    }
+  })
+}
+
+async function mostrarIntereses() {
+  const $divIntereses = document.querySelector('#intereses-ganados')
+  const intereses = await calcularPlazosFijos()
+
+  $divIntereses.innerHTML = `
+    <p>Con tus $${$montoUsuario.value} pesos, al mes ganarías:</p>
+    <ul>
+      ${intereses.map(r => `<li>${r.nombre}: $${r.rendimiento}</li>`).join('')}
+    </ul>
+  `
+}
+
+// evento
 const $botonCalcular = document.querySelector('#boton-calcular')
 
 $botonCalcular.addEventListener('click', () => {
   comprarDolares()
   comprarOro()
+  comprarDolarCripto()
   mostrarRendimientos()
+  mostrarIntereses()
 })
