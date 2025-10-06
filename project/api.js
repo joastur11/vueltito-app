d6cfc4d6a6129d610d9066fc35e1d4d965171fea
Joaquin Stur <joaquinstur@gmail.com>
2025-10-06 11:41:06 -0300

    call a json plazos fijos

--- project/api.js
+++ project/api.js
@@ -1,8 +1,14 @@
-export { cotizarDolares, obtenerPrecioOroUsd, obtenerTNA }
+export { cotizarDolares, cotizarDolarCripto, obtenerPrecioOroUsd, obtenerTNA, obtenerPlazosFijos }
 
 async function cotizarDolares() {
   const respuesta = await fetch('https://dolarapi.com/v1/dolares')
   if (!respuesta.ok) throw new Error('Error en el fetch Dolar api')
+  return await respuesta.json()
+}
+
+async function cotizarDolarCripto() {
+  const respuesta = await fetch('https://dolarapi.com/v1/dolares/cripto')
+  if (!respuesta.ok) throw new Error('Error en el fetch Dolar cripto')
   return await respuesta.json()
 }
 
@@ -28,5 +34,11 @@ async function obtenerPrecioOroUsd() {
 async function obtenerTNA() {
   const respuesta = await fetch('./tna.json')
   if (!respuesta.ok) throw new Error('Error cargando TNA')
+  return await respuesta.json()
+}
+
+async function obtenerPlazosFijos() {
+  const respuesta = await fetch('./plazos.json')
+  if (!respuesta.ok) throw new Error('Error cargando plazos fijos')
   return await respuesta.json()
 }
