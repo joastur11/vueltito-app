adaeb8748a57f0f2df662f85232e5adea63c0a3c
Joaquin Stur <joaquinstur@gmail.com>
2025-10-21 09:37:01 -0300

    va creando server

--- project/backend/index.js
+++ project/backend/index.js
@@ -1,13 +1,14 @@
 const express = require('express')
+const path = require('path')
 
 const PORT = process.env.PORT ?? 1234
 
 const app = express()
-app.use(express.json())
+app.use(express.static(path.join(__dirname, '..')))
 
 app.get('/', (req, res) => {
-  res.send('Servidor funcionando');
-});
+  res.sendFile(path.join(__dirname, '..', 'index.html'))
+})
 
 app.listen(PORT, () => {
   console.log(`Servidor corriendo en http://localhost:${PORT}`);
