da1d889358de0561576eb7ee8109987b74227905
Joaquin Stur <joaquinstur@gmail.com>
2025-10-21 09:51:08 -0300

    Update index.js

--- project/backend/index.js
+++ project/backend/index.js
@@ -1,10 +1,13 @@
 const express = require('express')
 const path = require('path')
+const fs = require('fs')
 
 const PORT = process.env.PORT ?? 1234
 
 const app = express()
 app.use(express.static(path.join(__dirname, '..')))
+
+const dataPath = path.join(process.cwd(), 'data', 'dolar.json')
 
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, '..', 'index.html'))
