e73920fd08108add50c75e6975b6c06f74d162f6
Joaquin Stur <joaquinstur@gmail.com>
2025-10-27 17:37:22 -0300

    funcion fetch historico
    la funcion hace el fetch por dia.

--- project/api.js
+++ project/api.js
@@ -1,4 +1,4 @@
-export { cotizarDolares, obtenerTNA, obtenerPlazosFijos }
+export { cotizarDolares, obtenerTNA, obtenerPlazosFijos, obtenerDolarPasado }
 
 async function cotizarDolares() {
   const respuesta = await fetch('https://dolarapi.com/v1/dolares')
@@ -18,18 +18,40 @@ async function obtenerPlazosFijos() {
   return await respuesta.json()
 }
 
-async function obtenerDolarPasado() {
+async function fetchYStorage() {
   const respuesta = await fetch('https://api.argentinadatos.com/v1/cotizaciones/dolares')
   if (!respuesta.ok) throw new Error('Error cargando plazos fijos')
   const data = await respuesta.json()
 
-  let fechaLimite = new Date()
+  const fechaLimite = new Date()
   fechaLimite.setMonth(fechaLimite.getMonth() - 1)
-
   const casasInteresadas = ['oficial', 'blue', 'cripto']
 
   const datosFiltrados = data.filter(d =>
-    casasInteresadas.includes(d.casa) && new Date(d.fecha) >= fechaLimite)
+    casasInteresadas.includes(d.casa) && new Date(d.fecha) >= fechaLimite
+  )
+
+  localStorage.setItem('dolarDatos', JSON.stringify(datosFiltrados))
+  localStorage.setItem('ultimaActualizacion', new Date().toISOString())
 
   return datosFiltrados
+}
+
+async function obtenerDolarPasado() {
+  const ultimaActualizacion = localStorage.getItem('ultimaActualizacion')
+
+  if (!ultimaActualizacion) {
+    return await fetchYStorage()
+  } else {
+    const fechaActual = new Date()
+    const fechaGuardada = new Date(ultimaActualizacion)
+    const diferenciaDias = (fechaActual - fechaGuardada) / (1000 * 60 * 60 * 24)
+
+    if (diferenciaDias > 1) {
+      return await fetchYStorage()
+    }
+    else {
+      return JSON.parse(localStorage.getItem('dolarDatos'))
+    }
+  }
 }
